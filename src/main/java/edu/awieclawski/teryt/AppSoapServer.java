4bb302fc30965037df56d59649501db2a4fcd7d2
AWieclawski <a_wieclawski@poczta.pl>
2023-06-11 13:48:47 +0200

    Teryt soap init

---
+++ src/main/java/edu/awieclawski/teryt/AppSoapServer.java
@@ -0,0 +1,11 @@
+package edu.awieclawski.teryt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSoapServer {
+	public static void main(String[] args) {
+		SpringApplication.run(AppSoapServer.class, args);
+	}
+}
